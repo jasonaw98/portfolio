c90bb10c30992060db7c456d60a51bef87b2e717
Jason <jason.aw@attspace.ventures>
2023-10-18 18:10:40 +0800

    Initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[20px] leading-[30px] text-justify'
+      >
+        As a seasoned software developer with expertise in TypeScript and JavaScript, I am passionate about creating efficient, scalable, and user-friendly solutions that solve real-world problems. With a proven track record of success in frameworks like React, Node.js, and Next.js, I am confident in my ability to collaborate closely with clients to bring their ideas to life. Additionally, I have extensive experience in blockchain development, having worked on projects involving Ethereum, Solana, and Substrate. Whether you're looking to build a new product or improve an existing one, I am dedicated to delivering high-quality results that exceed your expectations. Let's work together to make your vision a reality!
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10 justify-center'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
